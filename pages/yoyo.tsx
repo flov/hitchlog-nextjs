ac912bcbd1647b71714c01d49fed5a59e598e841
Florian Vallen <florian.vallen@gmail.com>
2022-07-03 02:00:04 +0200

    React fire, tailwindcss

---
+++ pages/yoyo.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Yoyo: NextPage = () => (
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+);
+
+export default Yoyo;
---
+++ src/utils/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCHCnUDEr21YboDgZctXoG4eLOj2x51YvM",
+  authDomain: "fir-test-4004d.firebaseapp.com",
+  databaseURL: "https://fir-test-4004d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-test-4004d",
+  storageBucket: "fir-test-4004d.appspot.com",
+  messagingSenderId: "276420004596",
+  appId: "1:276420004596:web:a0c4e8184b14a7d1352ca1",
+  measurementId: "G-JLQRYWCFQ4"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp);
+export const database = getDatabase(firebaseApp);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
