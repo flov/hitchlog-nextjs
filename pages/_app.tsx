006bf1abed2cb11d2893ca4440dbc1c8d53b086e
Florian Vallen <f.vallen@geton-institut.de>
2022-07-03 02:00:04 +0200

    React fire, tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { FirebaseAppProvider } from 'reactfire';
+import { firebaseConfig } from '../src/utils/firebase';
+import { createContext, Dispatch, SetStateAction, useState } from 'react';
+import { UserCredential } from 'firebase/auth';
+
+type UserContextType = {
+  user: UserCredential | null;
+  setUser: Dispatch<SetStateAction<UserCredential>>;
+} | null;
+
+export const UserContext = createContext<UserContextType>(null);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState(null);
+  return (
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Component {...pageProps} />
+      </UserContext.Provider>
+    </FirebaseAppProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/components/CurrentUser.tsx
@@ -0,0 +1,54 @@
+import { getAuth, signInWithPopup, AuthProvider, signOut } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { provider } from '../utils/firebase';
+
+const loginWithProvider = async (provider: AuthProvider) => {
+  const auth = getAuth();
+  try {
+    const { user } = await signInWithPopup(auth, provider);
+    console.log(user);
+  } catch (e) {
+    console.log(e);
+  }
+};
+const logOut = async () => {
+  const auth = getAuth();
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const CurrentUser = () => {
+  const auth = getAuth();
+  const [user, loading, error] = useAuthState(auth);
+  console.log(user);
+
+  if (loading) {
+    return (
+      <div>
+        <p>Initialising User...</p>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error}</p>
+      </div>
+    );
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Current User: {user.email}</p>
+        <button onClick={logOut}>Log out</button>
+      </div>
+    );
+  }
+  return (
+    <button onClick={() => loginWithProvider(provider)}>
+      Sign in with Google
+    </button>
+  );
+};
