7d882ab64915073adc8e5b75e88db04283a4db37
Florian Vallen <f.vallen@geton-institut.de>
2022-07-09 20:24:29 +0200

    Added Formik

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { FirebaseAppProvider } from 'reactfire';
 import { firebaseConfig } from '../src/utils/firebase';
-import { createContext, Dispatch, SetStateAction, useState } from 'react';
-import { UserCredential } from 'firebase/auth';
-
-type UserContextType = {
-  user: UserCredential | null;
-  setUser: Dispatch<SetStateAction<UserCredential>>;
-} | null;
-
-export const UserContext = createContext<UserContextType>(null);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [user, setUser] = useState(null);
   return (
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <UserContext.Provider value={{ user, setUser }}>
-        <Component {...pageProps} />
-      </UserContext.Provider>
+      <Component {...pageProps} />
     </FirebaseAppProvider>
   );
 }
