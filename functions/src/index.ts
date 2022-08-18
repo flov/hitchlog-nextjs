6a7df4d28aaf0ea686cb9b84f00f1d3e75b59613
Florian Vallen <florian.vallen@gmail.com>
2022-08-19 01:16:12 +0200

    ☁️ Firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,30 @@
 import * as functions from 'firebase-functions';
 import { Change, EventContext } from 'firebase-functions';
 import { QueryDocumentSnapshot } from 'firebase-functions/v1/firestore';
-import { getExperiencesForSnapshot } from './utils';
+import { getExperiencesForSnapshot, hasStoryForRidesSnapshot } from './utils';
 
 import admin = require('firebase-admin');
+
 admin.initializeApp();
+
+export const ridesOnCreate = functions.firestore
+  .document('/trips/{tripId}/rides/{rideId}')
+  .onWrite(
+    async (
+      change: Change<functions.firestore.DocumentSnapshot>,
+      context: EventContext
+    ) => {
+      const tripRef = change.after.ref.parent.parent;
+      if (!tripRef) return null;
+      const ridesSnapshot = await change.after.ref.parent.get();
+      const experiences = getExperiencesForSnapshot(ridesSnapshot);
+      const hasStory = hasStoryForRidesSnapshot(ridesSnapshot);
+      return tripRef.update({
+        hasStory,
+        experiences,
+      });
+    }
+  );
 
 export const ridesOnUpdate = functions.firestore
   .document('trips/{tripId}/rides/{rideId}')
@@ -14,9 +34,20 @@ export const ridesOnUpdate = functions.firestore
       const after = change.after.data();
       const tripRef = change.after.ref.parent.parent;
 
+      if (!tripRef) return null;
+
+      const trip = await tripRef.get();
+      if (before.story !== after.story) {
+        if (trip.exists) {
+          const ridesSnapshot = await change.after.ref.parent.get();
+          const hasStory = hasStoryForRidesSnapshot(ridesSnapshot);
+          const tripData = trip.data();
+          const updatedTrip = { ...tripData, hasStory };
+          functions.logger.log('has story', context.params.tripId, hasStory);
+          return tripRef.set(updatedTrip);
+        }
+      }
       if (before.experience !== after.experience) {
-        if (!tripRef) return null;
-        const trip = await tripRef.get();
         if (trip.exists) {
           const ridesSnapshot = await change.after.ref.parent.get();
           const experiences = getExperiencesForSnapshot(ridesSnapshot);
@@ -33,28 +64,3 @@ export const ridesOnUpdate = functions.firestore
       return null;
     }
   );
-
-exports.myFunction = functions.firestore
-  .document('trips/{tripId}/rides/{rideId}')
-  .onWrite(async (change, context) => {
-    const tripRef = change.after.ref.parent.parent;
-    if (!tripRef) return null;
-    const newRideData = change.after.data();
-    const tripData = await tripRef.get();
-    if (tripData.exists && newRideData?.experience) {
-      const ridesSnapshot = await change.after.ref.parent.get();
-      const experiences = getExperiencesForSnapshot(ridesSnapshot);
-      const updatedTrip = {
-        ...tripData.data(),
-        rideExperiences: experiences,
-      };
-      functions.logger.log(
-        'Updated Experiences of trip',
-        context.params.tripId,
-        experiences
-      );
-      return tripRef.set(updatedTrip);
-    }
-
-    return null;
-  });
