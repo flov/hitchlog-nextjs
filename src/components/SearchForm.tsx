6d633ac83a31a970ca7d5836db0cf19eb66a14b7
Florian Vallen <florian.vallen@gmail.com>
2022-10-15 22:36:30 +0100

    👀 Search Form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,20 +1,26 @@
-import { Button, Checkbox, Label, Select } from 'flowbite-react';
+import { Button, Checkbox, Label, Select, Spinner } from 'flowbite-react';
 import { Field, Form, FormikValues } from 'formik';
+import { FaCamera, FaScroll, FaVideo } from 'react-icons/fa';
 import { Experiences } from '../types';
 
-const SearchForm = ({ isSubmitting }: FormikValues) => {
+const SearchForm = ({
+  handleChange,
+  submitForm,
+  isSubmitting,
+  values,
+}: FormikValues) => {
   return (
-    <Form>
-      <div className="flex justify-between mb-2">
+    <Form className="flex justify-around sm:flex-row">
+      <div className="flex justify-between mb-2 gap-2">
         <Field name="rides_experience_eq">
           {({ field }: FormikValues) => (
-            <div className="w-48">
+            <div className="">
               <Select
                 id="rides_experience_eq"
                 name="rides_experience_eq"
                 {...field}
               >
-                <option value="">Select Experience</option>
+                <option value="">Experience</option>
                 {Experiences.map((experience) => (
                   <option key={experience} value={experience}>
                     {experience}
@@ -25,6 +31,23 @@ const SearchForm = ({ isSubmitting }: FormikValues) => {
           )}
         </Field>
 
+        <Field name="rides_photo_present">
+          {({ field }: FormikValues) => {
+            return (
+              <div className="flex items-center gap-2">
+                <Checkbox
+                  name="rides_photo_present"
+                  id="rides_photo_present"
+                  defaultChecked={field.value}
+                  {...field}
+                />
+                <Label htmlFor="rides_photo_present">
+                  <FaCamera />
+                </Label>
+              </div>
+            );
+          }}
+        </Field>
         <Field name="rides_story_present">
           {({ field }: FormikValues) => {
             return (
@@ -35,7 +58,9 @@ const SearchForm = ({ isSubmitting }: FormikValues) => {
                   defaultChecked={field.value}
                   {...field}
                 />
-                <Label htmlFor="rides_story_present">with story</Label>
+                <Label htmlFor="rides_story_present">
+                  <FaScroll className="inline" />
+                </Label>
               </div>
             );
           }}
@@ -50,13 +75,23 @@ const SearchForm = ({ isSubmitting }: FormikValues) => {
                 defaultChecked={field.value}
                 {...field}
               />
-              <Label htmlFor="rides_youtube_present">with video</Label>
+              <Label htmlFor="rides_youtube_present">
+                <FaVideo />
+              </Label>
             </div>
           )}
         </Field>
+      </div>
 
+      <div className="mb-4">
         <Button type="submit" disabled={isSubmitting}>
-          Search
+          {isSubmitting ? (
+            <div className="mr-2">
+              <Spinner light />
+            </div>
+          ) : (
+            <>Search</>
+          )}
         </Button>
       </div>
     </Form>
