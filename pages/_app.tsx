57e7d2be557002b9201870cc642f5d2e0c818597
Florian Vallen <florian.vallen@gmail.com>
2022-11-08 17:09:49 +0000

    ✅ Implements Logrocket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import { AuthProvider } from '../src/components/contexts/AuthContext';
 import ToastsProvider from '../src/components/contexts/ToastContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const logrocket = process.env.NEXT_PUBLIC_LOG_ROCKET;
+  logrocket && LogRocket.init(logrocket as string);
   return (
     <AuthProvider>
       <ThemeProvider attribute="class">
