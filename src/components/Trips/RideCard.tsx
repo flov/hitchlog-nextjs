77a7f200422e5d32e466bca92929e24ed3a671f0
Florian Vallen <florian.vallen@gmail.com>
2022-12-06 12:30:28 +0100

    💅 Ride Card updated styles

--- src/components/Trips/RideCard.tsx
+++ src/components/Trips/RideCard.tsx
@@ -13,37 +13,39 @@ export const RideCardHeader: FC<{
   trip_param: string;
 }> = ({ ride, trip_param }) => {
   return (
-    <div className="flex items-center justify-between mb-2">
-      <div className="flex items-center gap-2">
-        <Badge size="xs" color="purple">
-          {getOrdinalNumber(ride.number)}
-        </Badge>
-        <h2 className="text-xl font-bold text-gray-900 dark:text-white">
-          <Link href={`/trips/${trip_param}`}>{ride.title}</Link>
-        </h2>
+    <>
+      <div className="flex items-center justify-between mb-2">
+        <div className="flex items-center gap-2">
+          <Badge size="xs" color="purple">
+            {getOrdinalNumber(ride.number)}
+          </Badge>
+          <h2 className="text-xl font-bold text-gray-900 dark:text-white">
+            <Link href={`/trips/${trip_param}`}>{ride.title}</Link>
+          </h2>
+        </div>
+        <div className="flex items-center gap-2">
+          {ride.vehicle && vehicleToIcon(ride.vehicle)}
+          {ride.waiting_time && (
+            <Tooltip content="Waiting time">
+              <Badge size="xs" color="purple">
+                {ride.waiting_time}
+              </Badge>
+            </Tooltip>
+          )}
+          <Tooltip
+            content={`${getOrdinalNumber(ride.number)} Ride, ${
+              ride.experience
+            } Experience`}
+          >
+            <ExperienceCircle experience={ride.experience} size={4} />
+          </Tooltip>
+          <LikeRide ride={ride} />
+        </div>
       </div>
       <div className="flex items-center overflow-x-scroll gap-2">
-        <div className="flex items-center gap-2">
-          <TagsForRide ride={ride} />
-        </div>
-        {ride.vehicle && vehicleToIcon(ride.vehicle)}
-        {ride.waiting_time && (
-          <Tooltip content="Waiting time">
-            <Badge size="xs" color="purple">
-              {ride.waiting_time}
-            </Badge>
-          </Tooltip>
-        )}
-        <Tooltip
-          content={`${getOrdinalNumber(ride.number)} Ride, ${
-            ride.experience
-          } Experience`}
-        >
-          <ExperienceCircle experience={ride.experience} size={4} />
-        </Tooltip>
-        <LikeRide ride={ride} />
+        <TagsForRide ride={ride} />
       </div>
-    </div>
+    </>
   );
 };
 
