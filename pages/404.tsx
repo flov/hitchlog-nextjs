713a21c7b3a3437264268d773259afef13052324
Florian Vallen <florian.vallen@new-work.se>
2023-10-03 22:30:35 +0200

    🔧 custom 404 and 500 page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+const Custom404: NextPage = () => {
+  return (
+    <div className="max-w-4xl h-full text-center px-4 py-4 mx-auto align-center justify-center flex flex-col">
+      <h2 className="">404</h2>
+      <hr className="my-2 w-64 mx-auto" />
+      <p>Sometimes getting lost isn&apos;t so bad</p>
+    </div>
+  );
+};
+
+export default Custom404;
---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+const Custom500: NextPage = () => {
+  return (
+    <div className="max-w-4xl text-center px-4 py-4 mx-auto">
+      <h2>500 - Server-side error</h2>
+    </div>
+  );
+};
+
+export default Custom500;
