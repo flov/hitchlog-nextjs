66041b6c14c85ddd6b4bb01277999fb65cbdd42b
Florian Vallen <florian.vallen@new-work.se>
2023-10-06 14:41:58 +0200

    🔧 404 page text

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,7 @@ const Custom404: NextPage = () => {
     <div className="max-w-4xl h-full text-center px-4 py-4 mx-auto align-center justify-center flex flex-col">
       <h2 className="">404</h2>
       <hr className="my-2 w-64 mx-auto" />
-      <p>Sometimes getting lost isn&apos;t so bad</p>
+      <p>Not all who wonder are lost</p>
     </div>
   );
 };
